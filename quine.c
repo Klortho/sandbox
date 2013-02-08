c1788a1d5b67ca2bae1ec9329c276cd0cd265027
Chris Maloney <voldrani@gmail.com>
2013-02-07 23:24:39 -0500

    Add some newlines to quine.c

--- quine.c
+++ quine.c
@@ -1,1 +1,3 @@
-int main() { char *s = "int main() { char *s = %c%s%c; printf(s, 34, s, 34); }"; printf(s, 34, s, 34); }
+int main() { 
+    char *s = "int main() { char *s = %c%s%c; printf(s, 34, s, 34); }"; printf(s, 34, s, 34); 
+}
