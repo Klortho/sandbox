73a80d61183a53d1c4adeeb4accb097199918d32
Chris Maloney <voldrani@gmail.com>
2016-04-09 16:53:16 -0400

    ramda with raphael

---
+++ ramda/script.js
@@ -0,0 +1,19 @@
+var paper = Raphael("paper");
+
+var pp = p => `${p[0]} ${p[1]} `,
+    cmd = ltr => R.concat(ltr + ' '),
+    M = cmd('M'),
+    L = cmd('L'),
+    Z = 'Z';
+
+function triangle(color, v0, v1, v2) {
+  var path = M(pp(v0)) + 
+    L(pp(v1)) + 
+    L(pp(v2)) + 
+    Z;
+  var c = paper.path(path)
+    .attr({
+      fill: color
+    });
+}
+
